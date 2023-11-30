781e7fb359289652bb1211818833fd40ce535c9b
Huu Nguyen <kisscodes1909@gmail.com>
2023-11-30 21:14:30 +0700

    Catch all middleware for handling for unmatched routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
@@ -16,14 +17,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode;
 });
-
 // 3. Route.
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Catch-all middleware for handling unmatched routes
+// Since it lacks a specific path, this middleware will be triggered
+// for any request that doesn't match the paths defined earlier with app.use()
+// It returns a 404 error for routes that don't match previously defined routes.
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
--- 4-natours/starter/utils/apiFeatures.js
+++ 4-natours/starter/utils/apiFeatures.js
@@ -45,7 +45,7 @@ class APIFeatures {
 
   limitFields() {
     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',');
+      const fields = this.queryString.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
